203b064189f4e4f9591cab6304e04f815dd52e3f
Senad Alagic <senad.alagic@edu.fit.ba>
2022-07-06 19:12:13 +0200

    essentially finished

--- JS/Pretty Calculator/index.js
+++ JS/Pretty Calculator/index.js
@@ -1,3 +1,5 @@
+//add code to disable leading zeroes (boolean saying if the previous entry is a number or not)
+//add code to enable working values and a smoother experience
 function SendInput(input)
 {
     output.value+=input;
@@ -7,37 +9,30 @@ function Operation(key)
     number=parseInt(output.value);
     switch (key) 
     {
-        case '+':
-            workingValue+=number;
-            console.log(workingValue);
-            break;
-        case '-':
-            workingValue
-            console.log("-");
-            break;
-        case '*':
-            console.log("*");
-            break;
-        case '/':
-            console.log("/");
-            break;
         case '=':
-            console.log("=");
+            if(percentage!=0)
+            {
+                output.value=output.value*percentage/100;
+            }
+            else
+            {
+                output.value=(eval(output.value)!=null?eval(output.value):0);
+            }
             break;
         case 'C':
-            output.value="";
-            //console.log("C");
+            output.value=output.value.slice(0,output.value.length-1);
             break;
         case 'CE':
+            percentage=0;
             output.value="";
-            workingValue=0;
-            //console.log('CE');
+            //workingValue=0;
             break;
         case 'Inv':
             console.log('Inv');
             break;
         case '%':
-            console.log('%');
+            percentage=output.value;
+            output.value="";
             break;
         default:
             console.log("default case");
@@ -48,5 +43,6 @@ window.addEventListener('load',()=>
 {
     output=document.querySelector("#textoutput");
     output.value="";
-    workingValue=0;
+    percentage=0;
+    //workingValue=0;
 })
