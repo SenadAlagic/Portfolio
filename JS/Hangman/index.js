cea9f6a507fb7f51ae304ae7f4db478ebb1566f0
Senad Alagic <senad.alagic@edu.fit.ba>
2022-07-20 14:59:26 +0200

    undefined..

--- JS/Hangman/index.js
+++ JS/Hangman/index.js
@@ -1,3 +1,4 @@
+var selectedLetter="global right now";
 function GetLink()
 {
     fetch("https://random-word-api.herokuapp.com/word")
@@ -9,11 +10,14 @@ function GetLink()
             r.json().then(
                 c=>
                 {
-                    console.log(c,c.length);
                     for (let i=0; i<c[0].length; i++)
                     {
                         p=document.createElement("div");
                         p.innerHTML="_";
+                        if(i==0)
+                        {
+                            p.setAttribute("id","first");
+                        }
                         p.classList.add("word");
                         document.querySelector("#word").appendChild(p)
                     }
@@ -22,4 +26,18 @@ function GetLink()
         }
     )
 }
-GetLink();
+function SetupKeyboard()
+{
+    letters=document.getElementsByClassName("word");
+    //so much trouble was caused when doing selectedLetter=letters[0], always equates to undefined for some reason
+    selectedLetter=document.getElementById("first");
+}
+document.onkeydown= function(e) {
+    console.log(e.key);
+    selectedLetter.innerHTML=e.key;    
+}
+
+window.addEventListener('load', ()=>{
+    GetLink();
+    SetupKeyboard();
+});
