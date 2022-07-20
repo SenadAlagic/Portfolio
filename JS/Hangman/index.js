54f6fbe60e40d246c3cfd0315cefe4060e12ce8c
Senad Alagic <senad.alagic@edu.fit.ba>
2022-07-20 15:39:06 +0200

    fixed it but got bored, probably not coming back

--- JS/Hangman/index.js
+++ JS/Hangman/index.js
@@ -1,43 +1,29 @@
-var selectedLetter="global right now";
-function GetLink()
+async function GetLink() 
 {
-    fetch("https://random-word-api.herokuapp.com/word")
-    .then(
-        r=>
-        {
-            if(r.status!=200)
-                return;
-            r.json().then(
-                c=>
-                {
-                    for (let i=0; i<c[0].length; i++)
-                    {
-                        p=document.createElement("div");
-                        p.innerHTML="_";
-                        if(i==0)
-                        {
-                            p.setAttribute("id","first");
-                        }
-                        p.classList.add("word");
-                        document.querySelector("#word").appendChild(p)
-                    }
-
-                });
-        }
-    )
+    let r = await fetch("https://random-word-api.herokuapp.com/word");
+    if (r.status != 200)
+        return;
+    let c = await r.json();
+    for (let i = 0; i < c[0].length; i++) 
+    {
+        p = document.createElement("div");
+        p.innerHTML = "_";
+        p.classList.add("word");
+        document.querySelector("#word").appendChild(p)
+    }
 }
+
 function SetupKeyboard()
 {
     letters=document.getElementsByClassName("word");
     //so much trouble was caused when doing selectedLetter=letters[0], always equates to undefined for some reason
-    selectedLetter=document.getElementById("first");
+    selectedLetter=letters[0];
 }
+
 document.onkeydown= function(e) {
     console.log(e.key);
-    selectedLetter.innerHTML=e.key;    
 }
 
 window.addEventListener('load', ()=>{
     GetLink();
-    SetupKeyboard();
 });
